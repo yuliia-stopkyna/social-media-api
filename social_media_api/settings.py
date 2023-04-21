a82fe05339aefdf305b4f5fb5cefa3d7154e675a
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-04-21 15:20:53 +0300

    Create User and UserFollower models
    Change User creation with email and password instead of
    default Django username.
    Change first name and last name to required fields.
    Add bio, picture and country to the list of
    default AbstractUser fields.
    Add UserFollower model to implement follow and unfollow
    functionality.

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -116,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
