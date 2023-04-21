fe91416105f3ecbea336f9678b802fd46af2ee32
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-04-21 21:09:35 +0300

    Create logout endpoint
    Implement logout by blacklisting refresh token.

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
 
 from user.views import CreateUserView, ManageUserView
 
@@ -8,6 +12,7 @@ app_name = "user"
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("register/", CreateUserView.as_view(), name="create"),
-    path("profile/", ManageUserView.as_view(), name="update")
+    path("profile/", ManageUserView.as_view(), name="update"),
 ]
